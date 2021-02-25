8f8fcc61221fa5db0db852dbe577f126f333c782
kokoror <57328632+kokoror@users.noreply.github.com>
2021-02-24 19:40:00 -0800

    Create main.py

---
+++ true_false_quiz_oop/main.py
@@ -0,0 +1,28 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+from computer_data import computer_data
+
+#convert question_data to a list of object
+question_bank = []
+
+# for question in question_data:
+#     question_text = question['text']
+#     question_answer = question['answer']
+#     new_question = Question(question_text, question_answer)
+#     question_bank.append(new_question)
+
+#using data from opentbd.com
+for question in computer_data:
+    question_text = question['question']
+    question_answer = question['correct_answer']
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final socre is {quiz.score}/{quiz.question_number}")
